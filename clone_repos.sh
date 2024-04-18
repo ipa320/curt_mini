6a0439dbe8de81a69e3b693e770af83b108c4bf2
Dominik Moss <dominik.moss@ipa.fraunhofer.de>
2024-04-18 14:48:51 +0200

    Added recursive for submodule initialization in clone_repos

--- clone_repos.sh
+++ clone_repos.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # get curt mini repo
-vcs import < curt_mini/ipa_outdoor_robot_bringup.repos
+vcs import < curt_mini/ipa_outdoor_robot_bringup.repos --recursive
 # get all nav packages
 vcs import < ipa_outdoor_navigation/ipa_outdoor_navigation.repos
 # get necessary hardware packages
