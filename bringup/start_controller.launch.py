fbb500a35a647df2301540f31e01109c4f939261
Jonas Otto <jonas.otto@ipa.fraunhofer.de>
2025-02-14 10:23:56 +0100

    allow setting odom topic as launch argument

--- bringup/start_controller.launch.py
+++ bringup/start_controller.launch.py
@@ -1,12 +1,34 @@
 from launch import LaunchDescription
+import launch.substitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
+from launch_ros.parameter_descriptions import ParameterFile
+import typing
+
+
+class ResultingParameterFilePath(launch.substitution.Substitution):
+    """
+    Substitution which resolves to the path of the temporary file created by ParameterFile
+    """
+
+    def __init__(self, parameter_file: ParameterFile) -> None:
+        super().__init__()
+        self.__parameter_file = parameter_file
+
+    def perform(self, context: launch.launch_context.LaunchContext) -> typing.Text:
+        return self.__parameter_file.evaluate(context).absolute().as_posix()
+
 
 def generate_launch_description():
 
-    param_file = PathJoinSubstitution(
-        [FindPackageShare("curt_mini"), "config", "ros2_control.yaml"]
+    param_file = ResultingParameterFilePath(
+        ParameterFile(
+            PathJoinSubstitution(
+                [FindPackageShare("curt_mini"), "config", "ros2_control.yaml"]
+            ),
+            allow_substs=True,
+        )
     )
 
     robot_controller_spawner = Node(
