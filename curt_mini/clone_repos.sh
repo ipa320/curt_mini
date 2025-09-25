6fd52acc6c43aa72cfb406e5f7a4ad1029f1d233
curt <curt@ipa>
2025-09-25 17:31:50 +0200

    update repos files

--- curt_mini/clone_repos.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# get curt mini repo
-vcs import < curt_mini/ipa_outdoor_robot_bringup.repos --recursive
-# get all nav packages
-vcs import < ipa_outdoor_navigation/ipa_outdoor_navigation.repos --recursive
-# get necessary hardware packages
-vcs import < ipa_ros2_control/ipa_ros2_control.repos --recursive
--- curt_mini/hw_changelog.md
+++
@@ -1,3 +0,0 @@
-# HW Changes on the CURTmini
-
-[06.06.2025] Switched to Robosense Lidar RS-16
